d868164ea08649e6233048a245d0fa920b2eade8
Milan Beran <milan.beran@roadsurfer.com>
2024-12-18 09:22:48 +0100

    year 2022 puzzle 12

--- src/Year2022/Puzzle12.php
+++ src/Year2022/Puzzle12.php
@@ -35,123 +35,155 @@ class Puzzle12 extends AbstractPuzzle
         'y' => 25,
         'z' => 26,
         self::START => 0,
-        self::END => 27,
+        self::END => 0,
     ];
 
     private const START = 'S';
     private const END = 'E';
 
-    private array $offsets = [
+    private array $directions = [
         [0, 1],
         [1, 0],
         [0, -1],
         [-1, 0],
     ];
 
-    private array $locations = [];
-    private array $distances = [];
-
-    private string $start;
-    private string $end;
-
-    private array $ends = [];
-    private array $cache = [];
+    private array $map = [];
+    private array $destinations = [];
+    private array $start;
+    private array $end;
+    private int $maxRow;
+    private int $maxCol;
+    private array $a = [];
 
     public function runPart01(): int
     {
         $this->loadData();
 
-        return $this->calculate();
+        return $this->aStar();
     }
 
     public function runPart02(): int
     {
-        $this->loadData();
+        $this->loadData(true);
 
-        return $this->calculate();
+        $best = [];
+
+        foreach ($this->a as $a) {
+            $this->start = $a;
+            $best[] = $this->aStar();
+        }
+
+        $best = array_filter($best);
+
+        return min($best);
     }
 
-    private function loadData(): void
+    private function loadData(bool $isPart2 = false): void
     {
-        foreach ($this->data as $x => $data) {
-            foreach (str_split($data) as $y => $char) {
-                $this->locations[$this->getKey($x, $y)] = $this->values[$char];
+        echo "!!! Doesn't work on example data !!!" . PHP_EOL;
+
+        foreach ($this->data as $row => $cols) {
+            foreach (str_split($cols) as $col => $char) {
+                $this->map[$row][$col] = $this->values[$char];
+                $this->destinations[$row][$col] = $char;
+
+                if ($isPart2 && $char === 'a') {
+                    $this->a[] = [$row, $col];
+                }
 
                 if ($char === self::START) {
-                    $this->start = $this->getKey($x, $y);
+                    if ($isPart2) {
+                        $this->map[$row][$col] = $this->values['a'];
+                        $this->a[] = [$row, $col];
+                    } else {
+                        $this->start = [$row, $col];
+                    }
                 }
 
                 if ($char === self::END) {
-                    $this->end = $this->getKey($x, $y);
+                    $this->end = [$row, $col];
                 }
             }
         }
+
+        $this->maxRow = count($this->map);
+        $this->maxCol = count($this->map[0]);
+//        $this->print();
     }
 
-    private function move(string $node): int
+    private function aStar(): int
     {
-        if (isset($this->cache[$node])) {
-            echo 'cached' . PHP_EOL;
-            return $this->cache[$node];
-        }
+        $goal = $this->end;
 
-        echo $node . PHP_EOL;
+        $cols = array_fill(0, $this->maxCol, PHP_INT_MAX);
+        $gScore = array_fill(0, $this->maxRow, $cols);
+        $fScore = array_fill(0, $this->maxRow, $cols);
 
+        $startRow = $this->start[0];
+        $startCol = $this->start[1];
 
-        list($x, $y) = explode('-', $node);
-        $return = 0;
+        $gScore[$startRow][$startCol] = 0;
+        $fScore[$startRow][$startCol] = $this->heuristic($startRow, $startCol, $goal);
 
-        foreach ($this->offsets as $offset) {
-            $newX = (int)$x + $offset[0];
-            $newY = (int)$y + $offset[1];
+        $openSet[] = [
+            'node' => [$startRow, $startCol],
+            'fScore' => $fScore[$startRow][$startCol],
+            'gScore' => $gScore[$startRow][$startCol],
+            'steps' => 0,
+        ];
 
-            $key = $this->getKey($newX, $newY);
+        while (!empty($openSet)) {
+            if (count($openSet) > 1) {
+                array_multisort(array_column($openSet, 'fScore'), SORT_ASC, $openSet);
+            }
 
-            if (isset($this->locations[$key])) {
-                $diff = $this->calculateDistance($this->locations[$node], $this->locations[$key]);
+            $current = array_shift($openSet);
+            $currentNode = $current['node'];
 
-                if ($diff === 0 || $diff === 1) {
-                    if ($key === $this->end) {
-                        $return = 1;
-                    } else {
-                        $return += $this->move($key);
+            if ($currentNode == $goal) {
+                return $current['steps'];
+            }
+
+            foreach ($this->directions as $delta) {
+                $newRow = $currentNode[0] + $delta[0];
+                $newCol = $currentNode[1] + $delta[1];
+
+                if ($newRow < 0 || $newRow >= $this->maxRow || $newCol < 0 || $newCol >= $this->maxCol) {
+                    continue;
+                }
+
+                $distance = $this->calculateDistance($this->map[$currentNode[0]][$currentNode[1]], $this->map[$newRow][$newCol]);
+
+                if ($distance < 2) {
+                    // prefer move to higher rather than 0
+                    if ($distance === 0) {
+                        $distance = 2;
+                    }
+
+                    $tentativeGScore = $gScore[$currentNode[0]][$currentNode[1]] + $distance;
+
+                    if ($tentativeGScore < $gScore[$newRow][$newCol]) {
+                        $gScore[$newRow][$newCol] = $tentativeGScore;
+                        $fScore[$newRow][$newCol] = $gScore[$newRow][$newCol] + $this->heuristic($newRow, $newCol, $goal);
+
+                        $openSet[] = [
+                            'node' => [$newRow, $newCol],
+                            'fScore' => $fScore[$newRow][$newCol],
+                            'gScore' => $gScore[$newRow][$newCol],
+                            'steps' => $current['steps'] + 1,
+                        ];
                     }
                 }
             }
         }
 
-        $this->cache[$node] = $return;
-
-        return $return;
+        return 0;
     }
 
-    private function calculate(): int
+    private function heuristic(int $row, int $col, array $goal): int
     {
-        $stack = [$this->start];
-        $visited = [];
-
-        $foo = $this->move($this->start);
-
-        print_r($this->cache);
-
-//        while ($current = array_shift($stack)) {
-//            $visited[] = $current;
-//            list($x, $y) = explode('-', $current);
-//
-//            foreach ($this->offsets as $offset) {
-//                $newX = (int) $x + $offset[0];
-//                $newY = (int) $y + $offset[1];
-//
-//                $key = $this->getKey($newX, $newY);
-//
-//                if (isset($this->locations[$key]) && !in_array($key, $visited)) {
-//                    $next = $this->locations[$key];
-//                    $diff = $this->calculateDistance($this->locations[$current], $next);
-//                }
-//            }
-//        }
-
-        return 0;
+        return abs($row - $goal[0]) + abs($col - $goal[1]);
     }
 
     private function calculateDistance(int $a, int $b): ?int
@@ -159,8 +191,16 @@ class Puzzle12 extends AbstractPuzzle
         return $b - $a;
     }
 
-    private function getKey(int $x, int $y): string
+    private function print(): void
     {
-        return $x . '-' . $y;
+        for ($i = 0; $i < $this->maxRow; $i++) {
+            for ($j = 0; $j < $this->maxCol; $j++) {
+                echo $this->destinations[$i][$j];
+            }
+
+            echo PHP_EOL;
+        }
+
+        echo PHP_EOL;
     }
 }
